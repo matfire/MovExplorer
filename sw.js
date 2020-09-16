4a0cca843a2ac79393d971b5e2fe996f7f7e8695
Matteo Gassend <matteo.gassend@epitech.eu>
2020-09-16 10:52:56 +0200

    :sparkles: Caching for images, scripts and styles
    Service worker can now cache poster images for up to a week, and scripts and styles until a newer version can be download

--- sw.js
+++ sw.js
@@ -9,3 +9,30 @@ if (workbox) {
 self.addEventListener("fetch", (event) => {
   console.log("fetching")
 })
+
+// cache images coming from tmdb
+workbox.routing.registerRoute(({request}) => request.destination === "image",
+new workbox.strategies.CacheFirst({
+  cacheName: 'image-cache',
+  plugins: [
+    new workbox.cacheableResponse.CacheableResponsePlugin({
+      statuses: [0, 200]
+    }),
+    new workbox.expiration.ExpirationPlugin({
+      maxEntries: 100,
+      maxAgeSeconds: 7 * 24 * 60 * 60
+    })
+  ]
+}))
+
+workbox.routing.registerRoute(({request}) => request.destination === "style",
+new workbox.strategies.StaleWhileRevalidate({
+  cacheName: 'css-cache'
+})
+)
+
+workbox.routing.registerRoute(({request}) => request.destination === "script",
+new workbox.strategies.StaleWhileRevalidate({
+  cacheName: 'script-cache'
+})
+)
