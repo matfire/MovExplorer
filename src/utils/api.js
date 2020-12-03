96daa42ad1826716f2cee4aad12a511c8650e6c0
Matteo Gassend <matteo.gassend@epitech.eu>
2020-12-03 17:27:39 +0100

    :alien: Search returns details if limit is 1

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,10 @@ const moviedb = new MovieDb('2005b3a7fc676c3bd69383469a281eff')
 const searchMovie = async(query, limit=1) => {
     const res = await moviedb.searchMovie({query, })
     if (res.results.length > 0) {
-        return limit === 1 ? res.results[0] : res.results.slice(0, limit)
+        if (limit === 1) {
+            return await getMovieDetails(res.results[0].id)
+        }
+        return res.results.slice(0, limit)
     }
     return undefined
 }
